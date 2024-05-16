c774faabd42df64d47f193e49319c4f46aa3667f
lnormandon <lnormandon@openstudio.fr>
2024-05-16 09:56:21 +0200

    Fix models

---
+++ Model/HeaderHighlightsI18n.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HeaderHighlights\Model;
+
+use HeaderHighlights\Model\Base\HeaderHighlightsI18n as BaseHeaderHighlightsI18n;
+
+/**
+ * Skeleton subclass for representing a row from the 'header_highlights_i18n' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class HeaderHighlightsI18n extends BaseHeaderHighlightsI18n
+{
+
+}
---
+++ Model/HeaderHighlightsI18nQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HeaderHighlights\Model;
+
+use HeaderHighlights\Model\Base\HeaderHighlightsI18nQuery as BaseHeaderHighlightsI18nQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'header_highlights_i18n' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class HeaderHighlightsI18nQuery extends BaseHeaderHighlightsI18nQuery
+{
+
+}
